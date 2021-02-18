8f997aaa06d2229411fdd03f9d49a56cc73f3482
jdh5563 <56982973+jdh5563@users.noreply.github.com>
2021-02-17 23:25:48 -0500

    Fixed Player Movement
    Modified player movement so the player is bound to the constraints of the level.

--- Brackey-Jam-2021.1/Assets/Scripts/PlayerMovement.cs
+++ Brackey-Jam-2021.1/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,18 @@ public class PlayerMovement : MonoBehaviour
 	// The players position
 	private Vector3 position;
 
+	// The upper bounds of the level
+	private int xBound;
+	private int yBound;
+
     // Start is called before the first frame update
     void Start()
     {
+		xBound = GameManager.levelGrid.GetLength(1);
+		yBound = GameManager.levelGrid.GetLength(0);
+
 		// Sets the initial position of the player to be random
-		position = new Vector3(Random.Range(0, GameManager.levelGrid.GetLength(1)), Random.Range(0, GameManager.levelGrid.GetLength(0)), -1);
+		position = new Vector3(Random.Range(0, xBound), Random.Range(0, yBound), -1);
 		gameObject.transform.position = position;
     }
 
@@ -21,22 +28,22 @@ public class PlayerMovement : MonoBehaviour
 		// Moves the player in a direction
 		// that corresponds to the selected
 		// input
-		if (Input.GetKeyDown(KeyCode.W))
+		if (Input.GetKeyDown(KeyCode.W) && position.y < yBound - 1)
 		{
 			position.y += 1;
 		}
 
-		if (Input.GetKeyDown(KeyCode.S))
+		if (Input.GetKeyDown(KeyCode.S) && position.y > 0)
 		{
 			position.y -= 1;
 		}
 
-		if (Input.GetKeyDown(KeyCode.D))
+		if (Input.GetKeyDown(KeyCode.D) && position.x < xBound - 1)
 		{
 			position.x += 1;
 		}
 
-		if (Input.GetKeyDown(KeyCode.A))
+		if (Input.GetKeyDown(KeyCode.A) && position.x  > 0)
 		{
 			position.x -= 1;
 		}
