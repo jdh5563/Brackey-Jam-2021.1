cda25e2f0026556f61eea58029ab2373f54258fb
jdh5563 <56982973+jdh5563@users.noreply.github.com>
2021-02-17 23:17:09 -0500

    Implemented Character Movement
    Created a sample level in a grid format with a player that can move around.

---
+++ Brackey-Jam-2021.1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	// A 2D grid that represents the level
+	// Static so it can be accessed by other scripts
+	public GameObject gridEnd;
+	public static GameObject[,] levelGrid;
+
+    // Awake is called before any Start method
+    void Awake()
+    {
+		// Sets the size of the grid to be based on the top right corner
+		// tile since the position reflects the grid size minus 1
+		levelGrid = new GameObject[(int)gridEnd.transform.position.y + 1, (int)gridEnd.transform.position.x + 1];
+	}
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+}
---
+++ Brackey-Jam-2021.1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	// The players position
+	private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		// Sets the initial position of the player to be random
+		position = new Vector3(Random.Range(0, GameManager.levelGrid.GetLength(1)), Random.Range(0, GameManager.levelGrid.GetLength(0)), -1);
+		gameObject.transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Moves the player in a direction
+		// that corresponds to the selected
+		// input
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			position.y += 1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			position.y -= 1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			position.x += 1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			position.x -= 1;
+		}
+
+		// Adjusts the actual position of the character
+		gameObject.transform.position = position;
+    }
+}
